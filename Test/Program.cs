99d29b5ae96f88f09d5d0aaace7e5f58161474f8
John <bennyclarkup@gmail.com>
2019-06-25 03:27:10 -0500

    Final

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,8 +7,8 @@ namespace Test
         static void Main(string[] args)
         {//This is a horror story from many perspectives.May or may not be entertaining.
             Console.WriteLine("You stand inside a lobby as a nurse with crazy eyes but impecabble posture approaches you.");
-            Console.WriteLine(".");
-                Console.WriteLine("Welcome to the APSGE GMO Spa! Would you like to know more?");
+            Console.WriteLine("You think about leaving.");
+                Console.WriteLine("Welcome to the APSGE GMO Spa! Would you like to know more?, says the nurse.");
             //All People Suffer Genetic Errors
             string response;
             response = Console.ReadLine();
@@ -20,7 +20,7 @@ namespace Test
                 if (y == "physically")
                 {
                     Console.WriteLine("Wonderful. Just one small injection....");
-                    Console.WriteLine("You're dead. Don't trust doctors; especially if they carry needles. ");
+                    Console.WriteLine("You're dead. Don't trust doctors; especially if they carry needles.");
 
                 }
                 else
@@ -47,9 +47,9 @@ namespace Test
             else // annoyance
             {
                 Console.WriteLine("Have a nice day!");
-                Console.Write("As you walk away you hear screams come from the the windows and mutated semblance of a man briefly walks in front of a window.");
+                Console.Write("As you walk away you hear screams come from the the windows and the mutated semblance of a man briefly walks in front of a window.");
                 Console.Write("It; for no more was it kin to man; stood brow against window. It's eyes a yellow glow. The blind bestial rage evident.");
-                Console.WriteLine
+                Console.WriteLine("You run away down the street and aquire a lifelong problem with Iatrophobia.");
 
             }
 
